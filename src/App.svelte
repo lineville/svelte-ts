34ef1eaa8b83c42855acaa62c7e358c7854e0a88
Liam Neville <lneville@quantumscape.com>
2020-03-14 10:13:50 -0700

    adding speedtyper

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,7 @@
   import HanoiTowers from "./components/HanoiTowers.svelte";
   import SquareRootButton from "./components/SquareRootButton.svelte";
   import SquareRootBind from "./components/SquareRootBind.svelte";
+  import SpeedTyper from "./components/SpeedTyper.svelte";
 
   const count = writable(100);
   const unsubscribe = count.subscribe(value => console.log("count", value));
@@ -23,37 +24,37 @@
 
 <style>
   main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
 
 <main>
   <h1>Hello {name}!</h1>
   <p>
+    <SpeedTyper />
     <!-- * Uncomment any component you want to see -->
-    <SquareRootButton />
-    <SquareRootBind />
+    <!-- <SquareRootButton /> -->
+    <!-- <SquareRootBind /> -->
 
     <!-- <Counter /> -->
     <!-- <HanoiTowers /> -->
     <!-- <Counter value={1}>Counter 1</Counter>
     <Counter bind:value={$count} step={3}>Counter 2</Counter>
     <Counter bind:value={$myStore} step={5}>Counter 3</Counter> -->
-
 
   </p>
 </main>
