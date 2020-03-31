2ec464baad59b690e269751db319b0a5959865b4
lineville <lneville@quantumscape.com>
2020-03-31 15:18:09 -0700

    fixed domestic style
    Signed-off-by: lineville <lneville@quantumscape.com>

--- src/components/VirusStats/VirusDomestic.svelte
+++ src/components/VirusStats/VirusDomestic.svelte
@@ -1,32 +1,32 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import StatsTableDomestic from './StatsTableDomestic.svelte';
-  const apiURL = 'https://corona.lmao.ninja/states/';
+  import { onMount } from 'svelte'
+  import StatsTableDomestic from './StatsTableDomestic.svelte'
+  const apiURL = 'https://corona.lmao.ninja/states/'
 
-  let data = new Array<{ state: string }>();
-  let stateOptions = [];
-  let selectedState = 'New York';
-  let selectedStateData = {};
+  let data = new Array<{ state: string }>()
+  let stateOptions = []
+  let selectedState = 'New York'
+  let selectedStateData = {}
 
   const handleChange = async (e: any) => {
-    selectedState = e.target.value;
-    selectedStateData = data.filter(s => s.state === selectedState)[0] || {};
-  };
+    selectedState = e.target.value
+    selectedStateData = data.filter(s => s.state === selectedState)[0] || {}
+  }
 
   onMount(async () => {
-    console.log('Virus Stats Domestic Mounted');
+    console.log('Virus Stats Domestic Mounted')
     try {
-      let res = await fetch(apiURL);
-      data = await res.json();
+      let res = await fetch(apiURL)
+      data = await res.json()
       stateOptions = data.map((s: any, i: number) => ({
         id: i,
         state: s.state,
-      }));
-      selectedStateData = data.filter(s => s.state === selectedState)[0] || {};
+      }))
+      selectedStateData = data.filter(s => s.state === selectedState)[0] || {}
     } catch (e) {
-      console.error('error fetching data', e);
+      console.error('error fetching data', e)
     }
-  });
+  })
 </script>
 
 <style>
@@ -37,14 +37,11 @@
 </style>
 
 <div id="states-list">
-  <h5>
-    State:
-    <select on:change={handleChange} value={selectedState} class="countrySelector custom-select">
-      {#each stateOptions as option}
-        <option value={option.state}>{option.state}</option>
-      {/each}
-    </select>
-  </h5>
+  <select on:change={handleChange} value={selectedState} class="countrySelector custom-select">
+    {#each stateOptions as option}
+      <option value={option.state}>{option.state}</option>
+    {/each}
+  </select>
   <StatsTableDomestic {...selectedStateData} />
 
 </div>
