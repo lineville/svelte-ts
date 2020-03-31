3383e9a756a498ed5a5e0684c04a67dd88d82bc3
lineville <lneville@quantumscape.com>
2020-03-31 15:21:29 -0700

    removed spacing
    Signed-off-by: lineville <lneville@quantumscape.com>

--- src/components/VirusStats/VirusStats.svelte
+++ src/components/VirusStats/VirusStats.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  import VirusGlobal from './VirusGlobal.svelte';
-  import VirusDomestic from './VirusDomestic.svelte';
-  import VirusTimeline from './VirusTimeline.svelte';
-  import Switch from '../Shared/Switch.svelte';
-  let global = true;
-  let showTimeline = false;
+  import VirusGlobal from './VirusGlobal.svelte'
+  import VirusDomestic from './VirusDomestic.svelte'
+  import VirusTimeline from './VirusTimeline.svelte'
+  import Switch from '../Shared/Switch.svelte'
+  let global = true
+  let showTimeline = false
 </script>
 
 <style>
@@ -20,9 +20,6 @@
     <label for="globalSwitch">{global ? 'Global' : 'Domestic'}</label>
     <Switch bind:checked={global} />
   </div>
-
-  <br />
-
   {#if global}
     <VirusGlobal />
   {:else}
