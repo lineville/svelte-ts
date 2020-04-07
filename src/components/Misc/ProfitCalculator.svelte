b510abe9c8614a7870f96170ebd3244ca586e9b1
lineville <lneville@quantumscape.com>
2020-04-07 16:15:21 -0700

    improved stock prices
    Signed-off-by: lineville <lneville@quantumscape.com>

--- src/components/Misc/ProfitCalculator.svelte
+++ src/components/Misc/ProfitCalculator.svelte
@@ -2,7 +2,7 @@
   import { onMount } from 'svelte'
   import Chart from 'chart.js'
   import maxProfit from '../../utils/ProfitOptimizer'
-  export let prices: Array<number> = [1, 5, 10, 3, 4, 19, 2]
+  export let prices: Array<number> = [0]
   export let newPrice: number = 0
   export let availableBuys: number = 1
   let canvas: any
@@ -16,6 +16,7 @@
 
   const addPrice = (): void => {
     prices = [...prices, newPrice]
+    setProfit(prices, availableBuys)
     chart.destroy()
     chart = generateChart()
   }
@@ -51,6 +52,13 @@
     })
   }
 
+  const clearPrices = (): void => {
+    prices = [0]
+    setProfit(prices, availableBuys)
+    chart.destroy()
+    chart = generateChart()
+  }
+
   onMount(() => {
     chart = generateChart()
   })
@@ -71,9 +79,14 @@
   <hr />
 
   <div class="columns">
+
+    <div class="column">
+      <canvas id="stockChart" bind:this={canvas} />
+    </div>
+
     <div class="column">
       <div class="field is-horizontal">
-        <div class="field-label is-normal">
+        <div class="field-label is-expanded">
           <label class="label">Stock Price</label>
         </div>
         <div class="field-body">
@@ -85,14 +98,13 @@
                 <i class="fa fa-dollar-sign" />
               </span>
               <button class="button is-info" on:click={() => addPrice()}>Add to Stock Prices</button>
-
             </p>
           </div>
         </div>
       </div>
 
       <div class="field is-horizontal">
-        <div class="field-label is-normal">
+        <div class="field-label is-expanded">
           <label class="label"># of Trades</label>
         </div>
         <div class="field-body">
@@ -109,31 +121,23 @@
         </div>
       </div>
 
-      <div class="field is-horizontal">
-        <div class="field-label">
-          <!-- Left empty for spacing -->
-        </div>
-        <div class="field-body">
-          <div class="field">
-            <div class="control">
-              <button class="button is-primary" on:click={() => setProfit(prices, availableBuys)}>
-                Max Profit: {profit}
-              </button>
-            </div>
-          </div>
-        </div>
+      <div class="field is-grouped is-grouped-centered">
+
+        <p class="control">
+          <span class="tag is-primary is-large">Profit: $ {profit}</span>
+
+        </p>
+        <p class="control">
+          <button class="button is-light" on:click={clearPrices}>Clear</button>
+        </p>
       </div>
     </div>
 
     <div class="column">
-      <!-- <div class="chart-container" style="position: relative;"> -->
-      <canvas id="stockChart" bind:this={canvas} />
-      <!-- </div> -->
-    </div>
-
-    <div class="column">
       <table class="table is-hoverable" align="center">
-        <th>Stock Prices</th>
+        <th>
+          <div class="block">Stock Prices</div>
+        </th>
         {#each prices as price, day}
           <tr>
             <td>Day {day}</td>
